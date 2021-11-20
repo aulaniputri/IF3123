c3ca123f3440f47a2c6d877614075e216adf8a1a
aulaniputri <94121300+aulaniputri@users.noreply.github.com>
2021-11-20 20:38:24 +0700

    Add files via upload

---
+++ Unaryjmm.java
@@ -0,0 +1,12 @@
+public class Unaryjmm {
+	public static void main(String[] args) {
+		int i=10;
+		int j=3;
+		int k=0;
+
+		//cetaklah hasil operasi nilai k di console, berapa
+		//nilainya kemudian analisa
+		k= j-- +i;
+		System.out.println(k);
+	}
+}
